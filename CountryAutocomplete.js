9e9f05ebf9d66a953a3aa2bc95b2a7b01b3c3d97
Ilja <info@ilja-weber.de>
2019-07-11 11:08:43 +0200

    feat: write country codes to options

--- CountryAutocomplete.js
+++ CountryAutocomplete.js
@@ -1,59 +1,123 @@
 /**
  * Endereco SDK.
  *
- * @author Ilja Weber <ilja.weber@mobilemjo.de>
+ * @author Ilja Weber <ilja@endereco.de>
  * @copyright 2019 mobilemojo – Apps & eCommerce UG (haftungsbeschränkt) & Co. KG
  * {@link https://endereco.de}
  */
 function CountryAutocomplete(config) {
-    this.isSet = false;
-    this.config = config;
-    this.setElement = '';
-    this.connector = new XMLHttpRequest();
+
     var $self  = this;
-
-    setInterval( function() {
-        var isNowSet = false;
-        if((null !== document.querySelector(config.countrySelector))) {
-            isNowSet = true;
+    this.requestBody = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "getCountryCodes",
+        "params": {
+            "countryIds": []
         }
+    };
+    this.defaultConfig = {
+        'useWatcher': true,
+        'tid': 'not_set'
+    };
+    this.fieldsAreSet = false;
+    this.dirty = false;
+    this.config = Object.assign(this.defaultConfig, config);
+    this.connector = new XMLHttpRequest();
 
-        if (!$self.isSet && isNowSet) {
-            init();
-        } else if ($self.isSet && isNowSet) {
-            syncFields();
-        } else {
-            $self.isSet = false;
-        }
-    }, 500);
-
-    function init() {
-        console.log('initiate CountryWatcher');
-        $self.countryElement = document.querySelector($self.config.countrySelector);
-        $self.isSet = true;
+    /**
+     * Updates the config.
+     */
+    this.updateConfig = function(newConfig) {
+        $self.config = Object.assign($self.config, newConfig);
     }
 
-    function syncFields() {
-        if ('' === $self.countryElement.value) {
-            $self.countryElement.setAttribute('data-value', '');
-            return;
+    /**
+     * Checks if fields are set.
+     */
+    this.checkIfFieldsAreSet = function() {
+        var areFieldsSet = false;
+        if((null !== document.querySelector($self.config.countrySelector))) {
+            areFieldsSet = true;
         }
 
-        if ($self.setElement !== $self.countryElement.value) {
+        if (!$self.fieldsAreSet && areFieldsSet) {
+
+            // Fields are now set. Mark addresscheck as dirty to trigger reinitiation.
+            $self.dirty = true;
+            $self.fieldsAreSet = true;
+        } else if($self.fieldsAreSet && !areFieldsSet) {
+
+            // Fields have been removed.
+            $self.fieldsAreSet = false;
+        }
+    }
+
+    this.init = function() {
+        try {
+            $self.countryElement = document.querySelector($self.config.countrySelector);
+
+            // Define callback function.
+            $self.connector.onreadystatechange = function() {
+                if (4 === $self.connector.readyState) {
+                    if ($self.connector.responseText && '' !== $self.connector.responseText) {
+                        try {
+                            $data = JSON.parse($self.connector.responseText);
+                        } catch(e) {
+                            console.log('Parsing error', e);
+                        }
+
+                        $self.setCountryCodes($data.result.countryCodes);
+                    }
+                }
+            }
+
+            $self.requestBody.params.countryIds = $self.getCountryIds();
 
             $self.connector.abort();
-            $self.connector.open('POST', $self.config.countryEndpoint + '?id=' + $self.countryElement.value, false);
-            $self.connector.send();
-            $self.setElement = $self.countryElement.value;
+            $self.connector.open('POST', $self.config.countryEndpoint, true);
+            $self.connector.send((JSON.stringify($self.requestBody)));
+
+            $self.dirty = false;
+            console.log('CountryAutocomplete initiated.')
+        } catch(e) {
+            console.log('Could not initiate CountryAutocomplete because of error:', e);
         }
     }
 
-    // On data receive
-    this.connector.onreadystatechange = function() {
-        if (4 === $self.connector.readyState) {
-            if ($self.connector.responseText && '' !== $self.connector.responseText) {
-                $self.countryElement.setAttribute('data-value', $self.connector.responseText);
-            }
+    this.getCountryIds = function() {
+        var options;
+        var idsArray = {};
+        if (!$self.fieldsAreSet) {
+            return [];
+        }
+
+        options = document.querySelector($self.config.countrySelector).options;
+
+        for (var i = 0; i < options.length; i++) {
+            idsArray[i] = options[i].value;
+        }
+
+        return idsArray;
+    }
+
+    this.setCountryCodes = function(countryCodes) {
+        var options;
+        options = document.querySelector($self.config.countrySelector).options;
+        for (var i = 0; i < options.length; i++) {
+            options[i].setAttribute('data-code', countryCodes[i]);
         }
     }
+
+    // Service loop.
+    setInterval( function() {
+
+        if ($self.config.useWatcher) {
+            $self.checkIfFieldsAreSet();
+        }
+
+        if ($self.dirty) {
+            $self.init();
+        }
+    }, 300);
 }
